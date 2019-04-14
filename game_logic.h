e8e87d3f9ba43fb7a7bfcf3b9a05755868d5333a
L-Kerrigan <l.kerrigan@outlook.ie>
2019-04-14 15:05:26 +0100

    Adding game_logic library, currently unchanged

---
+++ game_logic.h
@@ -0,0 +1,50 @@
+/*
+ * File:   game_logic.h
+ * Author: Lucy
+ *
+ * Created on 08 March 2019, 11:52
+ */
+
+#ifndef GAME_LOGIC_H
+#define GAME_LOGIC_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GAME_LOGIC_H */
+
+/*
+ * Prints the board
+ *
+ * Input: the board to be printed.
+ */
+void print_board(square board[NUM_ROWS][NUM_COLUMNS]);
+
+
+/*
+ * Place tokens in the first column of the board
+ *
+ * Input: board - a 6x9 array of squares that represents the board
+ *        players - the array of the players
+ *        numPlayers - the number of players
+ */
+void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
+
+
+
+/*
+ *  * Manages the logic of the game
+ *
+ * Input: board - a 6x9 array of squares that represents the board
+ *        players - the array of the players
+ *        numPlayers - the number of players
+ */
+void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
