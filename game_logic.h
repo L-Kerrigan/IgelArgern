c9bec05f9ae9c2681ab3212f025965e018091b49
L-Kerrigan <l.kerrigan@outlook.ie>
2019-04-20 17:02:04 +0100

    Added obstacleSquares function

--- game_logic.h
+++ game_logic.h
@@ -1,50 +1,70 @@
 /*
- * File:   game_logic.h
- * Author: Lucy
- *
- * Created on 08 March 2019, 11:52
- */
+* File:   game_logic.h
+* Author: Lucy
+*
+* Created on 08 March 2019, 11:52
+*/
 
 #ifndef GAME_LOGIC_H
 #define GAME_LOGIC_H
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+  #endif
 
 
 
 
-#ifdef __cplusplus
+  #ifdef __cplusplus
 }
 #endif
 
 #endif /* GAME_LOGIC_H */
 
 /*
- * Prints the board
- *
- * Input: the board to be printed.
- */
-void print_board(square board[NUM_ROWS][NUM_COLUMNS]);
+* Returns the first letter associated with the color of the token
+*
+* Input: t - pointer to a token
+* Output: initial of the color of the token
+*
+*/
+char print_token(token *t);
 
+
+
+void printLine();
 
 /*
- * Place tokens in the first column of the board
- *
- * Input: board - a 6x9 array of squares that represents the board
- *        players - the array of the players
- *        numPlayers - the number of players
- */
+* Prints the board
+*
+* Input: the board to be printed.
+*/
+void print_board(square board[NUM_ROWS][NUM_COLUMNS]);
+/*
+* Place tokens in the first column of the board
+*
+* Input: board - a 6x9 array of squares that represents the board
+*        players - the array of the players
+*        numPlayers - the number of players
+*/
 void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
 
 
 
 /*
- *  * Manages the logic of the game
- *
- * Input: board - a 6x9 array of squares that represents the board
- *        players - the array of the players
- *        numPlayers - the number of players
- */
-void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
+*  * Manages the logic of the game
+*
+* Input: board - a 6x9 array of squares that represents the board
+*        players - the array of the players
+*        numPlayers - the number of players
+*/
+
+void chooseToken(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
+
+
+/*The function to be called whenever a token lands on its destination. It will check if said square is an obstacle,
+and if it is, it will take action. If not, nothing happens. */
+/*Maybe make this return a true/false value depending on whether the token can move,
+Then call the moveToken function in the play_game function if that token can be moved etc. */
+
+int obstacleSquares(square singleSquare);
