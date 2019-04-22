3e3f3e0acfa6a67225cb6cd67067aba339dc2dd7
L-Kerrigan <l.kerrigan@outlook.ie>
2019-04-22 19:00:05 +0100

    Added function to pop elements from stack of tokens

--- game_logic.c
+++ game_logic.c
@@ -107,7 +107,30 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
 }
 
 
+//Function to pop elements of stack. Inefficient but I can't figure out a better way right now
+int pop (tokenPtr *sPtr, int value){
+  if(value == (*sPtr)->col){
+    tokenPtr tempPtr = *sPtr;
+    *sPtr = (*sPtr)->next;
+    free(tempPtr);
+  }
+  else{
+    tokenPtr prevPtr = *sPtr;
+    tokenPtr currPtr = (*sPtr)->next;
 
+    while(currPtr!=NULL && currPtr->col!=value){
+      prevPtr = currPtr;
+      currPtr = currPtr->next;
+    }
+    if(currPtr!=NULL){
+      tokenPtr tempPtr = currPtr;
+      prevPtr->next=currPtr->next;
+      return value;
+    }
+  }
+
+  return '\0';
+}
 /*
 *  * Manages the logic of the game
 *
@@ -116,67 +139,101 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
 *        numPlayers - the number of players
 */
 
-void chooseToken(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers){
-  for (int i = 0; i < numPlayers; i++) {
-    srand(time(NULL)); //Randomising a number for the dice roll
-    int dice = (rand() % 6)+1;
+void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers){
+  do{
+    for (int i = 0; i < numPlayers; i++) {
+      srand(time(NULL)); //Randomising a number for the dice roll
+      int dice = (rand() % 5);
+      //This may be in the wrong order. Sidestep first then move forward???
+      int tileNum; //To hold the selected tile number
+      printf("Player %d, you rolled a %d. \n", i+1, dice);
+      printf("Which token will you move on row %d? Type the number of the tile it's on. \n", dice); //Has to be on the row the dice rolled
+      scanf("%d", &tileNum); //Player selects a the tile on which the token they want to move is on
+      if (tileNum < 0 || tileNum >= 8) { //Checks if the tile selected is within the range of the columns of the board
+        printf("This is an invalid tile number.\n");
+        //Need to find a way to ask the question again
+      } //Latter half of that statement shouldn't be required
+      else if(board[dice][tileNum].tokensOnSquare==0 || isEmpty(board[dice][tileNum].stack)) {
+        printf("There are no tokens on this square. Choose again. \n"); //Roll again?
+        i--; //Need to find a way to ask the question again. This method could be used for cheating
+      }
+      else if(board[dice][tileNum].type==OBSTACLE && (obstacleSquares(board[dice][tileNum])==0)){ //Need to check if the tile the token is on is an obstacle, not the future position!!
+        printf("Sorry, you miss a turn!\n\n"); //Is this right?
+      }
+      else{
+        char ans;
+        printf("Will you move your own token up or down? Enter Y or N.\n");
+        scanf(" %c", &ans);
 
-    /*Needs to be changed! Player can move any colour.*/
+        if (ans == 'Y' || ans == 'y') {
+          int tileCol, tileRow; //To hold the number of the tile the player wants to move his/her own token on
+          printf("Which token would you like to move? Type the row and column. \n");
+          scanf(" %d %d", &tileRow, &tileCol);
+          //This if statement also currently doesn't work, as above
+          /*if(board[tileRow][tileCol].stack != players[i].col)
+          {
+            printf("This is not your token. Choose again.\n");
+            //Again, need to figure out a way to ask the question
+          }
+          else{*/
+            char move;
+            printf("Press U for up or D for down.\n");
+            scanf(" %c", &move);
+            if (move == 'U' || move == 'u') { //This works to move it, but we need to ask which colour token the player wants to move
+              //This is just the code to move it
+              if(tileRow=0){
+                printf("Sorry, you can't move this token further up.\n");
+                //Need to add something that asks the question again. Or just leave it and let them move forward
+              }
+              else{
+                board[tileRow-1][tileCol].stack = (token *) malloc(sizeof (token));
+                board[tileRow-1][tileCol].stack->col = players[i].col;
+                board[tileRow-1][tileCol].tokensOnSquare++;
+                board[tileRow][tileCol].tokensOnSquare--;
+                pop(&(board[tileRow][tileCol].stack), board[tileRow][tileCol].stack->col);
+              }
+            }
+            else if (move == 'D' || move == 'd') {
+              if(tileRow=5){
+                printf("Sorry, you can't move this token further down.\n");
+                //Need to add something that asks the question again. Or just leave it and let them move forward
+              }
+              else{
+                //This is just the code to move it
+                board[tileRow+1][tileCol].stack = (token *) malloc(sizeof (token));
+                board[tileRow+1][tileCol].stack->col = players[i].col;
+                board[tileRow+1][tileCol].tokensOnSquare++;
 
-    int tileNum; //To hold the selected tile number
-    printf("Player %d, you rolled a %d. \n", i, dice);
-    printf("Which token will you move? Type the number of the tile it's on. \n"); //Has to be on the row the dice rolled
-    scanf("%d", &tileNum); //Player selects a the tile on which the token they want to move is on
-    if (tileNum <= 0 || tileNum >= 7) { //Not sure if this will actually cause the player to miss a turn. Need to check
-      printf("This is an invalid tile number.\n");
-      //exit(0);  //This is a void function, so I can't use return. Need to find a way to ask the question again
-    }
-    else if(board[tileNum][dice].type==OBSTACLE){ //Need to check if the tile the token is on is an obstacle, not the future position!!
-      obstacleSquares(board[tileNum][dice]);
-    }
-    char ans;
-    printf("Will you move your token up or down? Enter Y or N.\n");
-    scanf("%c", &ans);
-    if (ans == 'Y' || ans == 'y') {
-      char move;
-      printf("Press U for up or D for down.\n");
-      scanf("%c", &move);
-      if (move == 'U' || move == 'u') {
-        //Do we need to display where the token goes here? Or just move it?
-        //This is just the code to move it
-        if(tileNum=0){
-          printf("Sorry, you can't move this token further up.\n");
-          //Need to add something that asks the question again. Or just leave it and let them move forward
-        }
-        else{
-          tileNum-1;
+                board[tileRow][tileCol].tokensOnSquare--;
+                pop(&(board[tileRow][tileCol].stack), board[tileRow][tileCol].stack->col);
+              }
+            }
+            //Need to change stack of tokens on this square to display the one beneath the one that just moved
+            board[dice][tileNum+1].stack = (token *) malloc(sizeof (token));
+            board[dice][tileNum+1].stack->col = board[dice][tileNum].stack->col; //Need to change this to move the colour on the square previous forward
+            board[dice][tileNum+1].tokensOnSquare++; //Player also may choose their own colour ^^
+
+            board[dice][tileNum].tokensOnSquare--;
+            pop(&(board[dice][tileNum].stack), board[dice][tileNum].stack->col);
+
+          }
+          else if (ans == 'N' || ans == 'n') {
+            //Need to change stack of tokens on this square to display the one beneath the one that just moved
+            board[dice][tileNum+1].stack = (token *) malloc(sizeof (token));
+            board[dice][tileNum+1].stack->col = board[dice][tileNum].stack->col; //This will not work. Figure out how to implement stack properly
+            board[dice][tileNum+1].tokensOnSquare++;  //Player also may not choose their own colour ^^
+
+            board[dice][tileNum].tokensOnSquare--;
+            pop(&(board[dice][tileNum].stack), board[dice][tileNum].stack->col);
+          }
+          else{
+            printf("Invalid answer. \n");
+          }
+          print_board(board);
         }
       }
-      else if (move == 'D' || move == 'd') {
-        if(tileNum=6){
-          printf("Sorry, you can't move this token further down.\n");
-          //Need to add something that asks the question again. Or just leave it and let them move forward
-        }
-        else{
-          tileNum+1;
-        }
-        //Do we need to display where the token goes here? Or just move it?
-        //This is just the code to move it
-      }
-      board[tileNum][dice].stack = (token *) malloc(sizeof (token));
-      board[tileNum][dice].stack->col = players[i].col; //This will not work. Figure out how to implement stack properly
-      board[tileNum][dice].tokensOnSquare++; //Player also may not choose their own colour ^^
-    }
-    else if (ans == 'N' || ans == 'n') {
-      board[tileNum][dice].stack = (token *) malloc(sizeof (token));
-      board[tileNum][dice].stack->col = players[i].col; //This will not work. Figure out how to implement stack properly
-      board[tileNum][dice].tokensOnSquare++;  //Player also may not choose their own colour ^^
-    }
-    else{
-      printf("Invalid answer. \n");
-    }
-    print_board(board);
-  }
+    //}
+  }while((winning_player(players, numPlayers)==0)); //Keeps code running until a player has 3 tokens in the last column
 }
 
 //Divide this function up:
