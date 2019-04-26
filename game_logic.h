e860cefcf9bc518d2f0940ea80b8b4ca3b33580c
L-Kerrigan <l.kerrigan@outlook.ie>
2019-04-26 01:51:10 +0100

    Added function prototypes for new additions to game_logic.c

--- game_logic.h
+++ game_logic.h
@@ -1,10 +1,3 @@
-/*
-* File:   game_logic.h
-* Author: Lucy
-*
-* Created on 08 March 2019, 11:52
-*/
-
 #ifndef GAME_LOGIC_H
 #define GAME_LOGIC_H
 
@@ -12,14 +5,21 @@
 extern "C" {
   #endif
 
-
-
-
   #ifdef __cplusplus
 }
 #endif
 
 #endif /* GAME_LOGIC_H */
+
+//To check if the stack is empty. Returns 1 if the stack is empty
+int isEmpty(tokenPtr sPtr);
+
+//Function to add token to a stack
+int push(tokenPtr *sPtr, int value);
+
+//Function to delete token from a stack
+int pop(tokenPtr *sPtr, int value);
+
 
 /*
 * Returns the first letter associated with the color of the token
@@ -28,14 +28,10 @@ extern "C" {
 * Output: initial of the color of the token
 *
 */
-int isEmpty(tokenPtr sPtr);
-int push(tokenPtr *sPtr, int value);
-int pop(tokenPtr *sPtr, int value);
-
 char print_token(token *t);
 
 
-
+//Function to print a line
 void printLine();
 
 /*
@@ -44,6 +40,7 @@ void printLine();
 * Input: the board to be printed.
 */
 void print_board(square board[NUM_ROWS][NUM_COLUMNS]);
+
 /*
 * Place tokens in the first column of the board
 *
@@ -60,13 +57,18 @@ void place_tokens(square board[NUM_ROWS][NUM_COLUMNS], player players[], int num
 *        players - the array of the players
 *        numPlayers - the number of players
 */
-
 void play_game(square board[NUM_ROWS][NUM_COLUMNS], player players[], int numPlayers);
 
+//Function to increment the correct player's tokensOnLastCol when a token is added to a square in the 8th column
+void checkLastCol(square board[NUM_ROWS][NUM_COLUMNS], int row, player players[]);
 
-/*The function to be called whenever a token lands on its destination. It will check if said square is an obstacle,
-and if it is, it will take action. If not, nothing happens. */
-/*Maybe make this return a true/false value depending on whether the token can move,
-Then call the moveToken function in the play_game function if that token can be moved etc. */
-
+/*The function to be called whenever a token tries to move from an obstacle square.
+It will check if there are still tokens behind the square, and if there are, it will take action.
+If not, the obstacle square becomes a normal square. */
 int obstacleSquares(square board[NUM_ROWS][NUM_COLUMNS], int rows, int columns);
+
+//A function to check if any player has 3 tokens on the last column at the end of each round
+int winning_player(player players[], int numPlayers);
+
+//A function to move a token forward. Just to make things neater
+void moveTokenForward(square board[NUM_ROWS][NUM_COLUMNS], int row, int column);
