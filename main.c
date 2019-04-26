3613f10b56bd7ffee32c5f867816e3e89137ea7c
L-Kerrigan <l.kerrigan@outlook.ie>
2019-04-26 11:28:14 +0100

    Removed a few comments

--- main.c
+++ main.c
@@ -1,60 +1,9 @@
-/*
- * File:   main.c
- * Author: Lucy
- *
- * Created on 08 March 2019, 11:49
- */
-
-/*Check list for the game:
- - Initialise the board. Obstacle square positions already decided
- - Display the board - (Done)
- - Initialise (create) the players -(Done, to be checked)
- - Initialise struct for tokens -(Done, to be checked)
- - Make function to allow players to place tokens at start of game (Done, to be checked)
- - Make function to play game
- - Implement stack for tokens and make use of LIFO principle that it works on
-   so the token on top is the only one that can move
- - Implement obstacles squares so that the player can't move the token on that square
-   until all the others are level with it. The other tokens can only go as far as that token is
- - Edit main to put it all together -(Kinda done?)
- */
-
-
-
-
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-/*
- *
- */
-/*
-int main(int argc, char** argv) {
-
-    return (EXIT_SUCCESS);
-}
- */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
- * File:   main.c
- * Author: lpasqua
- *
- * Created on 06 March 2019, 12:11
- */
-
 #include "game_init.h"
 #include "game_logic.h"
 
-/*
- *
- */
 int main(int argc, char** argv) {
 
     //the board is defined as a 2-Dimensional array of squares
